ba174450060c6a38a2dfe9558c4fe402148db5ee
movie42 <44064122+movie42@users.noreply.github.com>
2020-11-24 13:17:14 +0900

    router and auth

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fBase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    authService.currentUser
+  );
+  return <AppRouter isLoggedIn={isLoggedIn} />;
+}
+
+export default App;
